1bfffb403fcb6f9f71d30e13f6dc54d445aa8879
651544537@qq.com <651544537@qq.com>
2022-09-27 15:26:27 +0800

    添加了一些注释

--- src/common/loginManager.java
+++ src/common/loginManager.java
@@ -28,14 +28,14 @@ public class loginManager extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		String userName = request.getParameter("userName");
-		String userPassword = request.getParameter("userPassword");
+		request.setCharacterEncoding("UTF-8");//对请求设定字符集
+		response.setContentType("text/html;charset=UTF-8");//对响应设定数据类型和字符集
+		PrintWriter out = response.getWriter();//获取响应对象的输出器
+		String userName = request.getParameter("userName");//获取请求中的用户名
+		String userPassword = request.getParameter("userPassword");//获取请求中的密码
 		if (!LoginConfirmer(userName, userPassword)) {
-			out.println("数据检索失败——登录失败！");
-		}
+			out.println("数据检索——登录失败！ERROR:LoginConfirmer return the result:false.");
+		}//传给登录验证器判断用户名和密码是否正确。
 		else {
 			out.println("欢迎登录，" + userName + "!<br/>");
 			out.println("您的密码为：" + userPassword + "<br/>");
